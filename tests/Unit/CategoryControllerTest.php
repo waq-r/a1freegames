541a734398dc0d0f4cc9b12ae4783cb60a1dba11
waqar3 <waqar3@gmail.com>
2021-08-01 21:14:54 +0100

    initialize home controller in category controller tests

--- tests/Unit/CategoryControllerTest.php
+++ tests/Unit/CategoryControllerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+
 
 class CategoryControllerTest extends TestCase
 {
@@ -14,12 +16,14 @@ class CategoryControllerTest extends TestCase
      */
     public function test_categoryExists()
     {
+        $hc = new HomeController();
     	$cc = new CategoryController();
         $this->assertTrue($cc->categoryExists('puzzles'));
     }
 
     public function test_getCategoryGames():void
     {
+        $hc = new HomeController();
     	$cc = new CategoryController();
     	$category = 'board';
     	$this->assertIsArray($cc->getCategoryGames($category));
